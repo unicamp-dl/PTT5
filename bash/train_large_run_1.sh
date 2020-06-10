c5faf7464e38ef50d785114e50b6dc055ff13b42
marcospiau <marcospiau123@gmail.com>
2020-06-10 23:22:26 +0000

    Added untracked files on first VM instance

---
+++ bash/train_large_run_1.sh
@@ -0,0 +1,7 @@
+python3 train.py \
+-b 1 \
+-n large_standard_vocab \
+-ms large \
+-e 1 \
+-s 512 
+
---
+++ bash/train_small_run_1.sh
@@ -0,0 +1,7 @@
+python3 train.py \
+-b 1 \
+-n small_standard_vocab \
+-ms small \
+-e 1 \
+-s 512 
+
