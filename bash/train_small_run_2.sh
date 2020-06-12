23f1ecc8a3fc1dce9d1af9d39e271e385664794d
marcospiau <marcospiau123@gmail.com>
2020-06-12 06:02:42 +0000

    Fixed script for run 2 of small standard

--- bash/train_small_run_2.sh
+++ bash/train_small_run_2.sh
@@ -4,5 +4,6 @@ python3 ../python/train.py \
 -ms small \
 -e 3 \
 -s 512 \
--jc '../assin/T5_configs_json/ptt5-standard-vocab-small-config.json'
+-jc '../assin/T5_configs_json/ptt5-standard-vocab-small-config.json' \
+-bp 'gs://ptt5-1/small_standard_vocab/models/'
 
