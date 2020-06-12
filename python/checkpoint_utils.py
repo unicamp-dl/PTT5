53f1ac0b78c44adfe31569277cce2376bc16a6cd
marcospiau <marcospiau123@gmail.com>
2020-06-12 04:31:34 +0000

    Now we can save checkpoints on GCS

--- python/checkpoint_utils.py
+++ python/checkpoint_utils.py
@@ -17,7 +17,7 @@ def get_checkpoint_step(ckpt_path):
     """Helper function to get step number for a given checkpoint
     Based on t5.models.mtf_model_get_latest_checkpoint_from_dir
 
-    Args: 
+    Args:
         ckpt_path: str, checkpoint path
 
     Returns:
@@ -28,32 +28,51 @@ def get_checkpoint_step(ckpt_path):
 def convert_tf_checkpoint_to_pytorch_gcs(tf_checkpoint_path, config_file,
                                          pytorch_dump_path):
     '''Wrapper for convert_tf_checkpoint_to_pytorch allowing checkpoints on GCS
-    
+
     Args:
         tf_checkpoint_path: str, path for tensorflow checkpoint
         config_file: str, The config json file corresponding to the pre-trained T5 model.
         pytorch_dump_path: str,  Path to the output PyTorch model.
     '''
-    
+
     files_to_download = tf.io.gfile.glob(tf_checkpoint_path + '.*')
 
     if not files_to_download:
         print(f'Checkpoint {tf_checkpoint_path} not found')
-        return 
-    
+        return
+
     with tempfile.TemporaryDirectory() as tmp_folder:
         for file in files_to_download:
             basename_file = os.path.basename(file)
             print(f"Downloading file {file} to {tmp_folder}")
             tf.io.gfile.copy(
-                file,os.path.join(tmp_folder, basename_file),
+                file,
+                os.path.join(tmp_folder, basename_file),
                 overwrite=True
             )
-        
+
         ckpt_files = os.path.join(tmp_folder,
                                   os.path.basename(tf_checkpoint_path))
-        convert_tf_checkpoint_to_pytorch(ckpt_files, config_file,
-                                         pytorch_dump_path)
+
+        if pytorch_dump_path.startswith('gs://'):
+            tmp_folder_ckpt = os.path.join(tmp_folder, '__TEMP__CHECKPOINT__')
+            os.makedirs(tmp_folder_ckpt)
+
+            pytorch_dump_basename = os.path.basename(pytorch_dump_path)
+            pytorch_dump_temp_path = os.path.join(tmp_folder_ckpt,
+                                                  pytorch_dump_basename)
+            convert_tf_checkpoint_to_pytorch(ckpt_files, config_file,
+                                            pytorch_dump_temp_path)
+
+            print("Save PyTorch model to {}".format(pytorch_dump_path))
+            tf.io.gfile.copy(
+                pytorch_dump_temp_path,
+                os.path.join(pytorch_dump_path),
+                overwrite=True
+            )
+        else:
+            convert_tf_checkpoint_to_pytorch(ckpt_files, config_file,
+                                            pytorch_dump_path)
 
 def compare_models(model_1, model_2):
     '''
@@ -72,30 +91,3 @@ def compare_models(model_1, model_2):
                 raise Exception
     if models_differ == 0:
         print('Models match perfectly! :)')
-
-def compare_params_hface_models(base, comp):
-    '''Compares two HuggingFace's  models
-
-    Compare two HuggingFace models, checking for:
-        - number of parameters
-        - state_dict keys and values
-    
-    Args: 
-        model1: HuggingFace model
-        model2: HuggingFace model
-
-    '''
-    return_dict = {}
-    # Number of parameters
-    print('match_num_params_only_trainable: {}'.format(
-    base.num_parameters(only_trainable=True) == \
-    comp.num_parameters(only_trainable=True)))
-
-    print('match_num_params_total: {}'.format(
-    base.num_parameters(only_trainable=True) == \
-    comp.num_parameters(only_trainable=True)))
-
-    # state dict
-    compare_models(base,comp)
-    
-    return  
