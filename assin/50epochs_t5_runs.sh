09fb61b0331a2b7535a69c1f6b64f56c01966b02
dscarmo <carmodiedre@outlook.com>
2020-06-18 00:42:13 -0300

    correct name

--- assin/50epochs_t5_runs.sh
+++ assin/50epochs_t5_runs.sh
@@ -1,4 +1,4 @@
-python3 t5_assin.py --name assin2_small_gen --model_name t5-small --vocab_name t5-small --bs 32 --architecture gen --max_epochs 50
-python3 t5_assin.py --name assin2_small_long --model_name t5-small --vocab_name t5-small --bs 32 --architecture long --max_epochs 50
-python3 t5_assin.py --name assin2_base_gen --model_name t5-base --vocab_name t5-base --bs 2 --architecture gen --max_epochs 50
-python3 t5_assin.py --name assin2_base_long --model_name t5-base --vocab_name t5-base --bs 2 --architecture long --max_epochs 50
+python3 t5_assin.py --name assin2_t5_small_gen --model_name t5-small --vocab_name t5-small --bs 32 --architecture gen --max_epochs 50
+python3 t5_assin.py --name assin2_t5_small_long --model_name t5-small --vocab_name t5-small --bs 32 --architecture long --max_epochs 50
+python3 t5_assin.py --name assin2_t5_base_gen --model_name t5-base --vocab_name t5-base --bs 2 --architecture gen --max_epochs 50
+python3 t5_assin.py --name assin2_t5_base_long --model_name t5-base --vocab_name t5-base --bs 2 --architecture long --max_epochs 50
