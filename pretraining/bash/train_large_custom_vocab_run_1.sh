024b4ec39c5a386ef601b53f05c20b9510f3b7c9
marcospiau <marcospiau123@gmail.com>
2020-06-21 08:17:37 +0000

    Added large custom vocab files

---
+++ pretraining/bash/train_large_custom_vocab_run_1.sh
@@ -0,0 +1,8 @@
+python3 ../python/train_custom_vocab.py \
+-b 1 \
+-n large_custom_sentencepiece_vocab \
+-ms large \
+-e 2 \
+-s 512 \
+-jc '../../assin/T5_configs_json/ptt5-standard-vocab-large-config.json' \
+-spm 'gs://ptt5-1/vocabs/spm_32000_unigram/spm_32000_pt.model'
