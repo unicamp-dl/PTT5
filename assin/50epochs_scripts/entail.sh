58c57646641ea72bbba469d84e97c6dd2225e68b
dscarmo <carmodiedre@outlook.com>
2020-07-04 01:09:31 -0300

    queue all 6 entail experiments

---
+++ assin/50epochs_scripts/entail.sh
@@ -0,0 +1,9 @@
+# Base
+python3 t5_assin.py assin2_t5_base_entail --model_name t5-base --vocab_name t5-base --bs 2 --architecture categoric --max_epochs 50 --nout 2
+python3 t5_assin.py assin2_ptt5_base_entail --model_name ptt5-4epoch-standard-vocab-base --vocab_name t5-base --bs 2 --architecture categoric --max_epochs 50 --nout 2
+python3 t5_assin.py assin2_ptt5_base_entail_custom --model_name ptt5-custom-vocab-base --vocab_name custom --bs 2 --architecture categoric --max_epochs 50 --nout 2
+
+# Small
+python3 t5_assin.py assin2_t5_small_entail --model_name t5-small --vocab_name t5-small --bs 32 --architecture categoric --max_epochs 50 --nout 2
+python3 t5_assin.py assin2_ptt5_small_entail --model_name ptt5-4epoch-standard-vocab-small --vocab_name t5-small --bs 32 --architecture categoric --max_epochs 50 --nout 2
+python3 t5_assin.py assin2_ptt5_small_entail_custom --model_name ptt5-custom-vocab-small --vocab_name custom --bs 32 --architecture categoric --max_epochs 50 --nout 2
--- assin/50epochs_scripts/t5_entail.sh
+++
@@ -1,1 +0,0 @@
-python3 t5_assin.py assin2_t5_base_entail --model_name t5-base --vocab_name t5-base --bs 2 --architecture categoric --max_epochs 50 --nout 2
