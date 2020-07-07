8ff606676311bad748b794b51564ac54f3356522
dscarmo <carmodiedre@outlook.com>
2020-07-07 12:52:01 -0300

    test entailment task

--- assin/final_test.py
+++ assin/final_test.py
@@ -3,13 +3,15 @@ from tqdm import tqdm
 from assin_dataset import ASSIN
 from t5_assin import T5ASSIN
 from scipy.stats import pearsonr
+from sklearn.metrics import f1_score
 
 
 if __name__ == "__main__":
     try:
         with torch.set_grad_enabled(False):
             result_str = ""
-            BEST_MODELS_PATHS = (("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
+            # Similarity models
+            '''BEST_MODELS_PATHS = (("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
                                   "assin2_t5_small_long/assin2_t5_small_long-epoch=26-val_loss=0.1042.ckpt"),
                                  ("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
                                   "assin2_t5_base_long/assin2_t5_base_long-epoch=14-val_loss=0.0626.ckpt"),
@@ -20,7 +22,20 @@ if __name__ == "__main__":
                                  ("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
                                   "assin2_ptt5_base_4epochs_long/assin2_ptt5_base_4epochs_long-epoch=21-val_loss=0.0407.ckpt"),
                                  ("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
-                                  "assin2_ptt5_base_long_custom_vocab/assin2_ptt5_base_long_custom_vocab-epoch=21-val_loss=0.0387.ckpt")
+                                  "assin2_ptt5_base_long_custom_vocab/assin2_ptt5_base_long_custom_vocab-epoch=21-val_loss=0.0387.ckpt"))'''
+            # Categoric models
+            BEST_MODELS_PATHS = (("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
+                                  "assin2_t5_small_entail/assin2_t5_small_entail-epoch=9-val_loss=0.2697.ckpt"),
+                                 ("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
+                                  "assin2_t5_base_entail/assin2_t5_base_entail-epoch=3-val_loss=0.1865.ckpt"),
+                                 ("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
+                                  "assin2_ptt5_small_entail/assin2_ptt5_small_entail-epoch=20-val_loss=0.2135.ckpt"),
+                                 ("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
+                                  "assin2_ptt5_small_entail_custom/assin2_ptt5_small_entail_custom-epoch=15-val_loss=0.1727.ckpt"),
+                                 ("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
+                                  "assin2_ptt5_base_entail/assin2_ptt5_base_entail-epoch=1-val_loss=0.1700.ckpt"),
+                                 ("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/models/"
+                                  "assin2_ptt5_base_entail_custom/assin2_ptt5_base_entail_custom-epoch=2-val_loss=0.1439.ckpt")
                                  )
             for BEST_MODEL_PATH in BEST_MODELS_PATHS:
                 print(f"Best model path: {BEST_MODEL_PATH}")
@@ -31,59 +46,113 @@ if __name__ == "__main__":
                 best_model = best_model.cuda()
                 assert not best_model.training
 
+                if "entail" in BEST_MODEL_PATH:
+                    print("Entailment model")
+                    ENTAIL = True
+                else:
+                    print("Similarity model")
+                    ENTAIL = False
+
                 print("Loading whole test dataset...")
-                if "custom" in BEST_MODEL_PATH.split('_'):
+                if "custom" in BEST_MODEL_PATH:
                     vocab_name = "custom"
-                elif "base" in BEST_MODEL_PATH.split('_'):
+                elif "base" in BEST_MODEL_PATH:
                     vocab_name = "t5-base"
-                elif "small" in BEST_MODEL_PATH.split('_'):
+                elif "small" in BEST_MODEL_PATH:
                     vocab_name = "t5-small"
 
                 bs = 48
                 total = 2448
                 assert total % bs == 0, "batch size has to be exactly divisible by total for this test"
 
-                print(f"Used vocabulary: {vocab_name}")
-                print(f"Fixed batch size: {bs}")
-                test_dataloader = ASSIN(mode="test",
-                                        version='v2',
-                                        seq_len=128,
-                                        vocab_name=vocab_name).get_dataloader(batch_size=bs, shuffle=False)
+                if ENTAIL:
+                    print(f"Used vocabulary: {vocab_name}")
+                    print(f"Fixed batch size: {bs}")
+                    test_dataloader = ASSIN(mode="test",
+                                            version='v2',
+                                            seq_len=128,
+                                            vocab_name=vocab_name,
+                                            categoric=True).get_dataloader(batch_size=bs, shuffle=False)
 
-                gold_standard = torch.zeros(total, dtype=torch.float32).cuda()
-                predictions = torch.zeros(total, dtype=torch.float32).cuda()
+                    gold_standard = torch.zeros(total, dtype=torch.float32).cuda()
+                    predictions = torch.zeros(total, dtype=torch.float32).cuda()
 
-                iterator = iter(test_dataloader)
-                iterator = tqdm(iterator, desc="Predicting...")
-                for n, x in enumerate(iterator):
-                    x = [i.cuda() for i in x]
-                    assert x[-1].shape[0] == bs, "dataloader didnt return correct batch size. Use a number divisible by the total!"
+                    iterator = iter(test_dataloader)
+                    iterator = tqdm(iterator, desc="Predicting...")
+                    for n, x in enumerate(iterator):
+                        x = [i.cuda() for i in x]
+                        assert x[-1].shape[0] == bs, "dataloader didnt return correct batch size. Use a number divisible by the total!"
 
-                    ix1 = bs*n
-                    ix2 = ((bs*n) + bs)
+                        ix1 = bs*n
+                        ix2 = ((bs*n) + bs)
 
-                    output = best_model(x).squeeze()
-                    predictions[ix1: ix2] = output
+                        output = best_model(x).squeeze()
+                        predictions[ix1: ix2] = output.argmax(dim=-1)
 
-                    target = x[-1]
-                    gold_standard[ix1: ix2] = target
+                        target = x[-1]
+                        gold_standard[ix1: ix2] = target
 
-                print(f"Predictions: {predictions}")
-                print(f"Gold Standards: {gold_standard}")
-                print(f"Predictions stats: max: {predictions.max()}, min: {predictions.min()}, shape: {predictions.shape}.")
-                print(f"Gold Standard stats: max: {gold_standard.max()}, min: {gold_standard.min()}, shape: {gold_standard.shape}.")
+                    predictions = predictions.detach().cpu().numpy()
+                    gold_standard = gold_standard.cpu().numpy()
+                    print(f"Predictions: {predictions}")
+                    print(f"Gold Standards: {gold_standard}")
+                    print(f"Predictions stats: max: {predictions.max()}, min: {predictions.min()}, shape: {predictions.shape}.")
+                    print(f"Gold Standard stats: max: {gold_standard.max()}, min: {gold_standard.min()}, shape: {gold_standard.shape}.")
 
-                # These implementations are exactly the same as the official ASSIN2 evaluation script
-                print("Calculating MSE...")
-                absolute_diff = gold_standard - predictions
-                mse = (absolute_diff ** 2).mean()
+                    # These implementations are exactly the same as the official ASSIN2 evaluation script
+                    print("Calculating Macro F1...")
+                    label_set = set(gold_standard)
+                    macro_f1 = f1_score(gold_standard, predictions, average='macro', labels=list(label_set))
 
-                print("Calculating Pearson...")
-                pearson = pearsonr(gold_standard.cpu().numpy(), predictions.detach().cpu().numpy())[0]
+                    print("Calculating accuracy...")
+                    accuracy = (gold_standard == predictions).sum() / len(gold_standard)
 
-                result_str += f"{BEST_MODEL_PATH} -  Pearson: {pearson}, MSE: {mse}\n"
-                print(f"Final test MSE: {mse}")
-                print(f"Final test Pearson: {pearson}")
+                    result_str += f"{BEST_MODEL_PATH} -  Macro F1: {macro_f1}, acc: {accuracy}\n"
+                    print(f"Final test Macro F1: {macro_f1}")
+                    print(f"Final test Accuracy: {accuracy}")
+                else:
+                    print(f"Used vocabulary: {vocab_name}")
+                    print(f"Fixed batch size: {bs}")
+                    test_dataloader = ASSIN(mode="test",
+                                            version='v2',
+                                            seq_len=128,
+                                            vocab_name=vocab_name,
+                                            categoric=False).get_dataloader(batch_size=bs, shuffle=False)
+
+                    gold_standard = torch.zeros(total, dtype=torch.float32).cuda()
+                    predictions = torch.zeros(total, dtype=torch.float32).cuda()
+
+                    iterator = iter(test_dataloader)
+                    iterator = tqdm(iterator, desc="Predicting...")
+                    for n, x in enumerate(iterator):
+                        x = [i.cuda() for i in x]
+                        assert x[-1].shape[0] == bs, "dataloader didnt return correct batch size. Use a number divisible by the total!"
+
+                        ix1 = bs*n
+                        ix2 = ((bs*n) + bs)
+
+                        output = best_model(x).squeeze()
+                        predictions[ix1: ix2] = output
+
+                        target = x[-1]
+                        gold_standard[ix1: ix2] = target
+
+                    print(f"Predictions: {predictions}")
+                    print(f"Gold Standards: {gold_standard}")
+                    print(f"Predictions stats: max: {predictions.max()}, min: {predictions.min()}, shape: {predictions.shape}.")
+                    print(f"Gold Standard stats: max: {gold_standard.max()}, min: {gold_standard.min()}, shape: {gold_standard.shape}.")
+
+                    # These implementations are exactly the same as the official ASSIN2 evaluation script
+                    print("Calculating MSE...")
+                    absolute_diff = gold_standard - predictions
+                    mse = (absolute_diff ** 2).mean()
+
+                    print("Calculating Pearson...")
+                    pearson = pearsonr(gold_standard.cpu().numpy(), predictions.detach().cpu().numpy())[0]
+
+                    result_str += f"{BEST_MODEL_PATH} -  Pearson: {pearson}, MSE: {mse}\n"
+                    print(f"Final test MSE: {mse}")
+                    print(f"Final test Pearson: {pearson}")
 
             with open("/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/final_tests.txt", 'w') as final_test_file:
                 final_test_file.write(result_str)
