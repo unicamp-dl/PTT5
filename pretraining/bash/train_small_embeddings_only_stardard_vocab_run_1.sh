16214aaef4f7eb9b4b7503527dd063a017a5f430
marcospiau <marcospiau123@gmail.com>
2020-07-09 03:16:37 +0000

    Updated train small standard vocab embedding only up to 2 epochs

--- pretraining/bash/train_small_embeddings_only_stardard_vocab_run_1.sh
+++ pretraining/bash/train_small_embeddings_only_stardard_vocab_run_1.sh
@@ -2,7 +2,7 @@ python3 ../python/train_v2.py \
 -b 1 \
 -n small_embeddings_only_standard_vocab \
 -ms small \
--e 1 \
+-e 2 \
 -s 512 \
 -jc '../../assin/T5_configs_json/ptt5-standard-vocab-small-config.json' \
 --train_embedding_only
