f965bcc8dad8ee79fd0129c0a664e3d814ecbc7c
dscarmo <carmodiedre@outlook.com>
2020-07-11 14:06:50 -0300

    text-to-text classification plots

--- assin/make_plots.py
+++ assin/make_plots.py
@@ -13,18 +13,21 @@ EXPERIMENT_SETS = {"small_long_gen": ("assin2_t5_small_long/version_0", "assin2_
                    "small_custom_standard": ("assin2_ptt5_small_4pochs_long/version_0", "assin2_ptt5_small_long_custom_vocab/version_0"),
                    "base_custom_standard": ("assin2_ptt5_base_4epochs_long/version_0", "assin2_ptt5_base_long_custom_vocab/version_0"),
                    "small_entail_custom_standard": ("assin2_ptt5_small_entail_custom/version_1", "assin2_ptt5_small_entail/version_1"),
-                   "base_entail_custom_standard": ("assin2_ptt5_base_entail_custom/version_1", "assin2_ptt5_base_entail/version_1")}
+                   "base_entail_custom_standard": ("assin2_ptt5_base_entail_custom/version_1", "assin2_ptt5_base_entail/version_1"),
+                   "small_entail_long_gen": ("assin2_t5_small_entail_acc/version_0", "assin2_t5_small_entail_gen/version_0"),
+                   "base_entail_long_gen": ("assin2_t5_base_entail_acc/version_0", "assin2_t5_base_entail_gen/version_0")}
 
 
 if __name__ == "__main__":
     MODES = ["train", "val", "test"]
     TAGS = [mode + "_loss" for mode in MODES]
 
-    BS = {"base": 32, "small": 2}
+    BS = {"base": 32, "small": 2, "large": 1}
     SIZE = {"train": 6500, "val": 500, "test": 2448}
 
     parser = argparse.ArgumentParser()
     parser.add_argument("experiment_set")
+    parser.add_argument("-val_acc", action="store_true")
     parser.add_argument('-l', "--log_folder", type=str,
                         default="/home/diedre/Dropbox/aUNICAMP/phd/courses/deep_learning_nlp/PTT5_data/logs")
 
@@ -35,22 +38,30 @@ if __name__ == "__main__":
         arch = "base" if "base" in experiment.split('_') else "small"
 
         for mode in ["train", "val"]:
+            experiment_dir = os.path.join(args.log_folder, experiment)
+
+            if args.val_acc and mode == "val":
+                metric_name = "val_acc"
+            else:
+                metric_name = f"{mode}_loss"
+
             if "t5" in experiment.split('_'):
-                label = f"{mode} linear layer" if "long/version" in experiment.split('_') else f"{mode} string generation"
+                if "long" in experiment or "acc" in experiment:
+                    label = f"{metric_name} linear layer"
+                else:
+                    label = f"{metric_name} string generation"
             else:
                 label = f"{mode} portuguese vocab" if "custom" in experiment else f"{mode} T5 vocab"
 
-            experiment_dir = os.path.join(args.log_folder, experiment)
-            df = tf_events_to_pandas(experiment_dir, f"{mode}_loss")
+            df = tf_events_to_pandas(experiment_dir, metric_name)
 
             size = SIZE[mode]
             bs = BS[arch]
             df["epoch"] = step_to_epoch(fix_step_offset(df["step"]), bs, size)
 
             plt.title(arch)
-            plt.ylabel(f"loss")
             plt.xlabel("epoch")
-            plt.plot(range(len(df[f"{mode}_loss"])), df[f"{mode}_loss"], label=label)
+            plt.plot(range(len(df[metric_name])), df[metric_name], label=label)
             plt.legend()
     plt.tight_layout()
     plt.show()
