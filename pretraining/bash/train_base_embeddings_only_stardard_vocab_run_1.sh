4abda5b91ab9991f9f76fa1e1aafbd3e8fc12924
marcospiau <marcospiau123@gmail.com>
2020-07-11 02:26:44 +0000

    Added files from base model with embeddings only standard vocab up to 4 epochs

---
+++ pretraining/bash/train_base_embeddings_only_stardard_vocab_run_1.sh
@@ -0,0 +1,8 @@
+python3 ../python/train_v2.py \
+-b 1 \
+-n base_embeddings_only_standard_vocab \
+-ms base \
+-e 4 \
+-s 512 \
+-jc '../../assin/T5_configs_json/ptt5-standard-vocab-base-config.json' \
+--train_embedding_only
