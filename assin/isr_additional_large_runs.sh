f047352895bfacdd888e5820dfd529bb6900dd5a
dscarmo <carmodiedre@outlook.com>
2020-07-13 12:16:27 -0300

    final experiments for arxiv

---
+++ assin/isr_additional_large_runs.sh
@@ -0,0 +1,7 @@
+# 2pochs large
+python3 t5_assin.py assin2_ptt5_large_entail_custom_vocab_10p_2poch --gpu 0 --model_name ptt5-custom-vocab-large-2poch --vocab_name custom --bs 1 --architecture categoric --max_epochs 50 --nout 2 --patience 10 --checkpoint_path ../checkpoints --model_path exp7 --log_path exp7
+
+# patience 10 entails
+python3 t5_assin.py assin2_t5_large_entail_10p --gpu 1 --model_name t5-large --vocab_name t5-large --bs 1 --architecture categoric --max_epochs 50 --nout 2 --patience 10 --checkpoint_path ../checkpoints --model_path exp8 --log_path exp8
+python3 t5_assin.py assin2_ptt5_large_entail_custom_vocab_10p --gpu 2 --model_name ptt5-custom-vocab-large --vocab_name custom --bs 1 --architecture categoric --max_epochs 50 --nout 2 --patience 10 --checkpoint_path ../checkpoints --model_path exp9 --log_path exp9
+python3 t5_assin.py assin2_ptt5_large_entail_10p --gpu 3 --model_name ptt5-standard-vocab-large --vocab_name t5-large --bs 1 --architecture categoric --max_epochs 50 --nout 2 --patience 10 --checkpoint_path ../checkpoints --model_path exp10 --log_path exp10
