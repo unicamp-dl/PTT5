c399243e042bdca7654da9e029fb29275f683d75
marcospiau <marcospiau123@gmail.com>
2020-07-15 02:03:50 +0000

    Added files from epoch 1 of large embedding only PT vocab

---
+++ pretraining/bash/train_large_embeddings_only_custom_vocab_run_1.sh
@@ -0,0 +1,9 @@
+python3 ../python/train_v2.py \
+-b 1 \
+-n large_embeddings_only_custom_sentencepiece_vocab \
+-ms large \
+-e 1 \
+-s 512 \
+-jc '../../assin/T5_configs_json/ptt5-standard-vocab-large-config.json' \
+--train_embedding_only \
+-spm 'gs://ptt5-1/vocabs/spm_32000_unigram/spm_32000_pt.model'
