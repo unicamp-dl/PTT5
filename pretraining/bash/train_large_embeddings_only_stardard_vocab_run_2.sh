96480fc24b3ea8268d3f8addbdb81fac7004c286
marcospiau <marcospiau123@gmail.com>
2020-07-16 00:37:23 +0000

    Added files from embeddings large standard vocab up to epoch 2

---
+++ pretraining/bash/train_large_embeddings_only_stardard_vocab_run_2.sh
@@ -0,0 +1,9 @@
+python3 ../python/train_v2.py \
+-b 1 \
+-n large_embeddings_only_standard_vocab \
+-ms large \
+-e 1 \
+-s 512 \
+-jc '../../assin/T5_configs_json/ptt5-standard-vocab-large-config.json' \
+--train_embedding_only \
+-bp 'gs://ptt5-1/large_embeddings_only_standard_vocab/models/'
