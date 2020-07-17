1d4e2f4248ed1e62aa1dfb363375c57db8f3da8d
dscarmo <carmodiedre@outlook.com>
2020-07-17 00:13:37 -0300

    faster 32 batch experiment

---
+++ assin/faster_32_batch.sh
@@ -0,0 +1,1 @@
+python3 t5_assin.py assin2_ptt5_base_acum_fast_long_custom_vocab --model_name ptt5-custom-vocab-base --vocab_name custom --bs 2 --acum 16 --architecture long --max_epochs 50 --lr 0.001
