72844c931e5920f6b04330c0470d100239a394da
marcospiau <marcospiau123@gmail.com>
2020-07-17 05:13:12 +0000

    Large embeddings only custom vocab up to epoch 2 finished

--- pretraining/bash/train_large_embeddings_only_custom_vocab_run_2.sh
+++ pretraining/bash/train_large_embeddings_only_custom_vocab_run_2.sh
@@ -6,4 +6,5 @@ python3 ../python/train_v2.py \
 -s 512 \
 -jc '../../assin/T5_configs_json/ptt5-standard-vocab-large-config.json' \
 --train_embedding_only \
--spm 'gs://ptt5-1/vocabs/spm_32000_unigram/spm_32000_pt.model' 
+-spm 'gs://ptt5-1/vocabs/spm_32000_unigram/spm_32000_pt.model' \
+-bp gs://ptt5-1/large_embeddings_only_custom_sentencepiece_vocab/models/
