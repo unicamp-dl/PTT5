2de1479ee41375af1880fa0cdb42053f456ff4f3
marcospiau <marcospiau123@gmail.com>
2020-07-21 03:53:02 +0000

    Finished epochs 1 and 2 of large batch size 128 standard vocab

---
+++ pretraining/bash/train_large_batchsize_128_standard_vocab_run_1.sh
@@ -0,0 +1,7 @@
+python3 ../python/train_v2.py \
+-b 0.5 \
+-n large_batchsize_128_standard_vocab \
+-ms large \
+-e 2 \
+-s 512 \
+-jc '../../assin/T5_configs_json/ptt5-standard-vocab-large-config.json' \
