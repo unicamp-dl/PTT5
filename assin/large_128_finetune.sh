2954ec55b3b1e463144267767643b9f5f210cad6
dscarmo <carmodiedre@outlook.com>
2020-07-24 12:54:50 -0300

    additional experiments

--- assin/large_128_finetune.sh
+++ assin/large_128_finetune.sh
@@ -1,5 +1,7 @@
 # Similarity
+python3 t5_assin.py assin2_ptt5_large_1282_long_custom_vocab --model_name ptt5-custom-vocab-128-large --vocab_name custom --bs 1 --acum 2 --architecture long --max_epochs 50 --checkpoint_path checkpoints --model_path models --log_path logs
 python3 t5_assin.py assin2_ptt5_large_128_long_custom_vocab --model_name ptt5-custom-vocab-128-large --vocab_name custom --bs 1 --architecture long --max_epochs 50 --checkpoint_path checkpoints --model_path models --log_path logs
 
 # Classification
+python3 t5_assin.py assin2_ptt5_large_1282_entail_custom_vocab --model_name ptt5-custom-vocab-128-large --vocab_name custom --bs 1 --acum 2 --architecture categoric --max_epochs 50 --nout 2 --checkpoint_path checkpoints --model_path models --log_path logs --patience 10
 python3 t5_assin.py assin2_ptt5_large_128_entail_custom_vocab --model_name ptt5-custom-vocab-128-large --vocab_name custom --bs 1 --architecture categoric --max_epochs 50 --nout 2 --checkpoint_path checkpoints --model_path models --log_path logs --patience 10
