15396ed24334d06333d6f9e42e4613a045642eb5
marcospiau <marcospiau123@gmail.com>
2020-07-25 04:39:35 +0000

    Added files from large batch size 128 custom vocab epochs 3 and 4

---
+++ pretraining/bash/train_large_batchsize_128_custom_vocab_run_2.sh
@@ -0,0 +1,9 @@
+python3 ../python/train_v2.py \
+-b 0.5 \
+-n large_batchsize_128_custom_sentencepiece_vocab \
+-ms large \
+-e 2 \
+-s 512 \
+-jc '../../assin/T5_configs_json/ptt5-standard-vocab-large-config.json' \
+-spm 'gs://ptt5-1/vocabs/spm_32000_unigram/spm_32000_pt.model' \
+-bp 'gs://ptt5-1/large_batchsize_128_custom_sentencepiece_vocab/models'
