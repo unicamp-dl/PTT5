238a19c5464c341e24499742cf65e0060cb68905
marcospiau <marcospiau123@gmail.com>
2020-07-25 01:49:45 +0000

    Added files from large batch size 128 standard vocab epochs 3 and 4

---
+++ pretraining/bash/train_large_batchsize_128_standard_vocab_run_2.sh
@@ -0,0 +1,8 @@
+python3 ../python/train_v2.py \
+-b 0.5 \
+-n large_batchsize_128_standard_vocab \
+-ms large \
+-e 2 \
+-s 512 \
+-jc '../../assin/T5_configs_json/ptt5-standard-vocab-large-config.json' \
+-bp 'gs://ptt5-1/large_batchsize_128_standard_vocab/models/'
