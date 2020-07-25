3419ff000cecba11d7456dc67fd9b235d5d98917
marcospiau <marcospiau123@gmail.com>
2020-07-24 23:13:45 -0300

    Added flag for saving all checkpoints on pretraining

--- pretraining/python/train_v2.py
+++ pretraining/python/train_v2.py
@@ -79,6 +79,11 @@ parser.add_argument(
         action='store_true',
         help='Flag to train only embeddings and freeze everything else'
         )
+parser.add_argument(
+        '--keep_all_checkpoints',
+        action='store_true',
+        help='Flag to keep all checkpoints ('
+        )
 
 args = parser.parse_args()
 
@@ -249,7 +254,7 @@ model = t5.models.MtfModel(
     learning_rate_schedule=0.003,
     save_checkpoints_steps=5000,
     # keep_checkpoint_max=keep_checkpoint_max if ON_CLOUD else None,
-    keep_checkpoint_max=keep_checkpoint_max,
+    keep_checkpoint_max=None if keep_all_checkpoints else keep_checkpoint_max,
     #how many batches of data are sent to TPU in a "training loop"
     iterations_per_loop=100,
     variable_filter=fn_is_var_embedding if args.train_embedding_only else None
