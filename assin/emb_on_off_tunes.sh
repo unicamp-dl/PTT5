9575cf148870d14ec82595435b6c9696e45b8643
dscarmo <carmodiedre@outlook.com>
2020-07-27 17:36:29 -0300

    emb experiments start

---
+++ assin/emb_on_off_tunes.sh
@@ -0,0 +1,5 @@
+python3 t5_assin.py assin2_ptt5_01emb_base_long_custom_vocab --model_name ptt5-0.1epoch-custom-vocab-emb-base --vocab_name custom --bs 2 --architecture long --max_epochs 50
+python3 t5_assin.py assin2_ptt5_01_base_long_custom_vocab --model_name ptt5-0.1epoch-custom-vocab-base --vocab_name custom --bs 2 --architecture long --max_epochs 50
+
+python3 t5_assin.py assin2_ptt5_1emb_base_long_custom_vocab --model_name ptt5-1.0epoch-custom-vocab-emb-base --vocab_name custom --bs 2 --architecture long --max_epochs 50
+python3 t5_assin.py assin2_ptt5_1_base_long_custom_vocab --model_name ptt5-1.0epoch-custom-vocab-base --vocab_name custom --bs 2 --architecture long --max_epochs 50
