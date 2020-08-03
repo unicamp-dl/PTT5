77102f4d69f412a647f8c204b75041d4ccc2831d
dscarmo <carmodiedre@outlook.com>
2020-08-03 13:11:41 -0300

    last base experiment

---
+++ assin/base_entail.sh
@@ -0,0 +1,1 @@
+python3 t5_assin.py assin2_ptt5_base_emb_entail_custom_vocab --model_name ptt5-custom-vocab-emb-base --vocab_name custom --bs 2 --architecture categoric --nout 2 --max_epochs 50 --patience 10
